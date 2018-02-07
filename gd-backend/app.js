27c783ff28f69305d8652deae12a168944aef9ee
Daniel Hook <danielhook@Daniels-MBP-2.localdomain>
2018-02-07 10:15:08 -0800

    update

--- gd-backend/app.js
+++ gd-backend/app.js
@@ -14,3 +14,29 @@ app.use(cors())
 app.get('/home', (req, res) => {
   res.json({ message: 'api example app' })
 })
+
+app.get('/login', (req, res) => {
+  res.json({ message: 'login test' })
+})
+
+app.post('/dates/new' (req, res) => {
+  Dates.findAll().then()
+})
+
+app.get('/dates' (req, res) => {
+  Dates.findAll().then(dates => {
+    res.json({ dates: dates })
+  })
+})
+
+app.post('/activity/new', (req, res) => {
+  Activity.create({
+    title: req.body.title,
+    cost: req.body.cost,
+    location: req.body.location,
+    description: req.body.description
+  }).then((activity)=>{
+    res.status(201)
+    res.json({activity: activity})
+  })
+})
