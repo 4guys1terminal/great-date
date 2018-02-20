5dddc1ffbabb23b05cac62539e6ce3dbbf634c26
Jake Parolin <jakeparolin@Jakes-MBP.localdomain>
2018-02-20 15:47:47 -0800

    deploying

--- gd-backend/app.js
+++ gd-backend/app.js
@@ -6,7 +6,8 @@ const crypto = require('crypto')
 var sequelize = require('sequelize');
 const Op = sequelize.Op
 const fs = require('fs');
-var path = require('path');
+
+
 
 var app = express();
 
@@ -22,9 +23,6 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true,}));
 app.use(validator());
 app.use(cors());
-
-const staticFiles = express.static(path.join(__dirname, '../../gd-frontend/build'))
-app.use(staticFiles)
 
 // authorization token
 const authorization = (req, res, next) => {
@@ -48,10 +46,9 @@ const authorization = (req, res, next) => {
         res.json({message: 'Authorization Token Required'})
     }
 }
-
 // homepage
 app.get('/', (req, res) => {
-    res.json({message: 'Backend is running, we gucci'});
+    res.json({message: 'API example app'});
 });
 
 // displays respective route  w/ raw json from database onto page
@@ -88,47 +85,79 @@ app.get('/activities/:id', (req, res) => {
     });
 });
 
-// TODO: FE browse page lets user pick a number of certain tags
-// tags are sent to BE on req.body.tags
-// sequelize queries the database for all activities that match those tags
-// returns all activityId's that match the chosen tags to FE via:
-// res.json({browseActivities: browseActivities}
 
-//route for browse page generation ALL ACTIVITY IDs BY TAG
-app.post('/browse', (req, res) => {
-    tags = req.body.tags
-    let tagArr = []
-    for (var property in tags) {
-        tags[property] === true
-            ? tagArr.push(parseInt(property))
-            : ''
-    }
+// logic for browse page generation ALL ACTIVITY IDs BY TAG
+// app.post('/', (req, res) => {
+//   tags = req.body.tags
+//   let tagArr = []
+//   for (var property in tags) {
+//
+//       tags[property] === true
+//           ? tagArr.push(parseInt(property))
+//           : ''
+//   }
+//   // console.log('tagArr',tagArr);
+//
+//
+//   Tags.sequelize.query(`SELECT * FROM "Activities" JOIN "ActivityTags" ON "Activities".id="ActivityId"  WHERE "TagId" IN (${tagArr});`, {type: sequelize.QueryTypes.SELECT})
+//   .then(shuffle => {
+//     let browseTags = []
+//     for (var i = 0; i < shuffle.length; i++) {
+//       browseTags.push(shuffle[i].id)
+//     }
+//     // console.log(browseTags);
+//     res.json({browseTags: browseTags})
+//   })
+//   // .then(activity => {
+//   //   console.log("activity",activity);
+//   // })
+// });
 
-    Tags.sequelize.query(`SELECT * FROM "Activities" JOIN "ActivityTags" ON "Activities".id="ActivityId"  WHERE "TagId" IN (${tagArr});`, {type: sequelize.QueryTypes.SELECT}).then(allBrowseActivities => {
-        let browseActivityIds = []
+// logic for random generator
 
-        for (var i = 0; i < allBrowseActivities.length; i++) {
-            console.log(allBrowseActivities[i].id);
-            browseActivityIds.push(allBrowseActivities[i].ActivityId)
+//
 
-        }
-        console.log(browseActivityIds);
-        res.json({browseActivityIds: browseActivityIds})
-    })
-});
 
-// route for random date generator
+
+
+
+
+
+// logic for random generator
+// app.post('/', (req, res) => {
+//     tags = req.body.tags
+//     console.log('tags',tags);
+//     let tagArr = []
+//     for (var property in tags) {
+//         tags[property] === true
+//             ? tagArr.push(parseInt(property))
+//             : ''
+//     }
+//     console.log('tagArr',tagArr);
+//
+//
+//     Tags.sequelize.query(`SELECT * FROM "Activities" JOIN "ActivityTags" ON "Activities".id="ActivityId"  WHERE "TagId" IN (${tagArr});`, {type: sequelize.QueryTypes.SELECT})
+//       .then(shuffle => {
+//         console.log("shuffle.id",shuffle.id);
+//         let randomTag = shuffle[Math.floor(Math.random() *shuffle.length)].ActivityId
+//         console.log('random', randomTag);
+//         res.status(201)
+//         res.json({randomTag: randomTag})
+//     })
+// });
+
 app.post('/', (req, res) => {
     tags = req.body.tags
     let tagArr = []
-
     for (var property in tags) {
+
         tags[property] === true
             ? tagArr.push(parseInt(property))
             : ''
     }
 
-    Tags.sequelize.query(`SELECT * FROM "Activities" JOIN "ActivityTags" ON "Activities".id="ActivityId"  WHERE "TagId" IN (${tagArr});`, {type: sequelize.QueryTypes.SELECT}).then(shuffle => {
+    Tags.sequelize.query(`SELECT * FROM "Activities" JOIN "ActivityTags" ON "Activities".id="ActivityId"  WHERE "TagId" IN (${tagArr});`, {type: sequelize.QueryTypes.SELECT})
+    .then(shuffle => {
         // console.log(shuffle[0]);
         let randomTag = shuffle[Math.floor(Math.random() * shuffle.length)].ActivityId
         res.status(201)
@@ -137,7 +166,9 @@ app.post('/', (req, res) => {
 });
 
 
+
 //Creating New User   (Need Kevin and Dan to comment)
+
 app.post('/users', (req, res) => {
 
     req.checkBody('firstName', 'Is required').notEmpty()
@@ -154,7 +185,7 @@ app.post('/users', (req, res) => {
                 res.json({message: 'success', user: user})
             })
         } else {
-            // console.log(valErrors.array())
+            // console.log(validationErrors.array())
             res.status(400)
             res.json({
                 errors: {
@@ -173,12 +204,8 @@ app.post('/activities', (req, res) => {
     req.checkBody('description', 'is required').notEmpty()
     req.checkBody('location', 'is required').notEmpty()
     req.checkBody('cost', 'is required').notEmpty()
-
-
-    // TODO: Get these validations working
-
-    // req.checkBody('tag','is required').notEmpty()
-    // req.checkBody('imageFile', 'is required').notEmpty()
+    // req.checkBody('tag','is required').notEmpty() --------               Need to
+    // req.checkBody('imageFile', 'is required').notEmpty() ---------   Figure these out
 
     // if there are no errors logged, then it allows the activity to be created
     req.getValidationResult().then((validationErrors) => {
@@ -187,22 +214,22 @@ app.post('/activities', (req, res) => {
             let fileType = req.body.imageType[0]
 
             let ext = fileType.split('/')
-            let extension = '.' + ext[(ext.length - 1)]
+            let extension = '.' + ext[(ext.length-1)]
 
-            // hashing the image content to store as the name, with the activity (to avoid duplicate name problem)
+            // hashing the image name to store with the activity (to avoid duplicate name problem)
             let hashedImageContent = crypto.createHash('md5').update(fileContent).digest('hex');
-            // console.log(hashedImageContent);
+            console.log(hashedImageContent);
             //converting base64 string back into an image and saving to /user-uploads/ folder
             let images = req.body.imageFile.map((image) => {
 
                 const base64ToImage = require('base64-to-image');
 
-                let path = './public/user-uploads/'
-                let details = {
+                var path = './public/user-uploads/'
+                var optionalObj = {
                     'fileName': hashedImageContent
                 };
 
-                base64ToImage(image, path, details)
+                base64ToImage(image, path, optionalObj)
             })
 
             Activity.create({
@@ -210,7 +237,7 @@ app.post('/activities', (req, res) => {
                 description: req.body.description,
                 location: req.body.location,
                 cost: req.body.cost,
-                imageName: hashedImageContent + extension,
+                imageName: hashedImageContent+extension,
             }).then((activity) => {
                 res.status(201)
                 res.json({activity: activity})
@@ -243,6 +270,8 @@ app.post('/activities', (req, res) => {
         }
     })
 })
+
+
 
 // login form
 app.post('/sessions/new', (req, res) => {
@@ -287,8 +316,9 @@ app.post('/sessions/new', (req, res) => {
 //     })
 // })
 
-//TODO: put route for editing activities
-//
+// put route for editing activities
+
+// has this been worked on at all really? -JD 2/15/2018
 app.put('/activities/edit/:id', (req, res) => {
 
     const {name, content} = req.params;
@@ -317,6 +347,5 @@ app.get('/login', authorization, function(req, res) {
     res.json({user: request.currentUser})
 })
 
-app.use('/*', staticFiles)
 
 module.exports = app
