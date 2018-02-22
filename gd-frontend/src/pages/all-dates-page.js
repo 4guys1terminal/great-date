a3c2f5006f1c58d85b54b240ba8fc12288daefff
Daniel Hook <danielhook@Daniels-MBP-2.localdomain>
2018-02-21 16:51:04 -0800

    fix api stuff

--- gd-frontend/src/pages/all-dates-page.js
+++ gd-frontend/src/pages/all-dates-page.js
@@ -25,6 +25,8 @@ import fetches from '../functions/fetch.js';
 
 const {fetchActivity, fetchTags} = fetches()
 
+const host = process.env.NODE_ENV === 'production' ? 'https://the-great-date-app.herokuapp.com' : 'http://localhost:3000'
+
 var backgroundTexture = {
     backgroundImage: 'url(/images/grid_noise.png)'
 };
