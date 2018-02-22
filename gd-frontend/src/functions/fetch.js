2218cf6a0a0d3c949a970eb6f378248b0e6f6cf4
Daniel Hook <danielhook@Daniels-MBP-2.localdomain>
2018-02-21 16:23:21 -0800

    fix fetch funcs

--- gd-frontend/src/functions/fetch.js
+++ gd-frontend/src/functions/fetch.js
@@ -1,6 +1,3 @@
-var host
- if(process.env.NODE_ENV === 'production'){ host = "/" } else { host = "http://localhost:3000/" }
-
 const host = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
 
 export default function(host) {
