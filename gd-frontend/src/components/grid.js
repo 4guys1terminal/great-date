25e52622d55d9d0f67eb0e503e4cfce468b1a7f8
Daniel Hook <danielhook@Daniels-MBP-2.localdomain>
2018-02-22 16:03:09 -0800

    deploying

--- gd-frontend/src/components/grid.js
+++ gd-frontend/src/components/grid.js
@@ -8,7 +8,7 @@ const { fetchTags, fetchActivities } = api
 
 const host = process.env.NODE_ENV === 'production' ? 'https://the-great-date-app.herokuapp.com' : 'http://localhost:3000'
 
-const path = "public/user-uploads/"
+const path = "/public/user-uploads/"
 const imgSrc = imageFactory(host, path)
 
 
