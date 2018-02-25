fcbdadc763b96f7342770426be8e055a11a53aa2
Daniel Hook <danielhook@Daniels-MBP-2.localdomain>
2018-02-24 17:18:32 -0800

    ...

--- gd-frontend/src/functions/imgSrc.js
+++ gd-frontend/src/functions/imgSrc.js
@@ -1,4 +1,4 @@
 // this function takes a host url and path to the image storage and returns the img source path
 export default function imageFactory(url, image){
-    return url + image // test
+    return url + image
 }
