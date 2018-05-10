d6b522d2976581ac484ee8e6c52a62c20e601a7b
Jordan Dominguez <jordan@yellowdogsolutions.com>
2018-05-10 11:25:00 -0700

    Minor organization

--- gd-frontend/src/App.js
+++ gd-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/home';
 import LogInPage from './pages/log-in-page';
@@ -15,44 +15,46 @@ import CreateDateRedirect from './pages/create-date-redirect'
 
 import ScrollToTop from './functions/scrollToTop.js';
 
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            newUserSuccess: false,
-            errors: null,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      newUserSuccess: false,
+      errors: null
+    };
+  }
 
-    render() {
-        return (
-        <Router>
-            <Switch>
-                <ScrollToTop>
+  render() {
+    return (<Router>
+      <Switch>
+        <ScrollToTop>
 
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/home' component={Home}/>
-                    <Route path='/activities/:id' component={DatePage}/>
-                    <Route path='/login-page' component={LogInPage}/>
-                    <Route path='/all-dates-page' component={AllDatesPage}/>
-                    <Route path='/new-activity' component={NewActivityPage}/>
-                    <Route path='/success' component={NewActivitySuccess}/>
-                    <Route path='/logged-in-page' component={LoggedInPage}/>
-                    <Route exact path="/sign-up-page" render={props => (<div>
-                            <SignUpPage errors={this.state.errors && this.state.errors.validations}/>
-                        </div>)}/>
-                    <Route path='/date-generator-page' component={DateGeneratorPage}/>
-                    <Route path='/create-date-redirect' component={CreateDateRedirect}/>
+          <Route exact="exact" path='/' component={Home}/>
+          <Route path='/home' component={Home}/>
+          <Route path='/activities/:id' component={DatePage}/>
+          <Route path='/login-page' component={LogInPage}/>
+          <Route path='/all-dates-page' component={AllDatesPage}/>
+          <Route path='/new-activity' component={NewActivityPage}/>
+          <Route path='/success' component={NewActivitySuccess}/>
+          <Route path='/logged-in-page' component={LoggedInPage}/>
+          <Route exact="exact" path="/sign-up-page" render={props => (<div>
+              <SignUpPage errors={this.state.errors && this.state.errors.validations}/>
+            </div>)}/>
+          <Route path='/date-generator-page' component={DateGeneratorPage}/>
+          <Route path='/create-date-redirect' component={CreateDateRedirect}/>
 
-                  {/* TODO: create components for the login and dashboard */}
-                    <Route path='/admin-login' />
+          {/* TODO: create components for the login and dashboard */}
+
+          {/* <Route path='/admin-login' />
                     <Route path='/admin-dash' />
-                </ScrollToTop>
-            </Switch>
-        </Router>);
-    }
+                   */
+          }
+
+        </ScrollToTop>
+      </Switch>
+    </Router>);
+  }
 }
 
 export default App;
